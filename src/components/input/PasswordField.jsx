1224cae9dbe86380dc7bd24d85c452331347aa8f
Febin Baiju <febin.baiju@orisys.in>
2021-06-05 17:14:19 +0530

    Added TextField component

---
+++ src/components/input/PasswordField.jsx
@@ -0,0 +1,78 @@
+import { Col, Form, Row } from "react-bootstrap"
+
+export const PasswordField = ({
+    name,
+    value,
+    defaultValue,
+    onChange,
+    setValue,
+    required,
+    label,
+    placeholder,
+    readonly = false,
+    sameRow = true,
+    customParameters = {},
+    textBelow,
+    isValid,
+    validClass,
+    invalidClass,
+    ...props
+}) =>{
+    const field = <>
+    <Form.Control
+    {
+        ...customParameters?.field
+    }
+    type="password"
+    {...(isValid === true ? { className: validClass } : { className : invalidClass })}
+    value={value}
+    readOnly={readonly}
+    placeholder={placeholder}
+    {...(defaultValue ?  {defaultValue : defaultValue } : null )}
+    onChange={setValue}
+    />
+    {
+        textBelow &&
+        <Form.Text
+        {
+            ...customParameters?.textbelow
+        }
+        >
+        {textBelow}
+        </Form.Text>
+    }
+    </>
+
+    return(
+    <Form.Group
+    {
+        ...customParameters?.group
+    }
+    {...(sameRow ? { as: Row} : null )}
+    >
+    { label ? 
+    <Form.Label
+    {
+        ...customParameters?.label
+    }
+    {...(sameRow ? { column: true } : null )} 
+    >
+      {label}
+    </Form.Label>
+    :
+    null
+    }
+    { sameRow ?
+    <Col
+    {
+        ...customParameters?.column
+    }
+    >
+    {field}
+    </Col>
+    :
+    field
+    }
+       </Form.Group>
+        )
+}
