5d6c469f17aea5620b6c6c7c201e3298b932b35e
Febin Baiju <febin129@gmail.com>
2021-06-12 14:43:26 +0530

    Added BaseForm to test form

---
+++ src/apps/forms/BaseForm.js
@@ -0,0 +1,45 @@
+import { useFormik } from "formik";
+import { Button, Form } from "react-bootstrap";
+import { TextField } from '../../components/input/TextField'
+import { PasswordField } from '../../components/input/PasswordField'
+
+export default function BaseForm(props) {
+
+    let customParameters = {
+        "group" : {
+            className : "mb-3"
+        },
+        "label" : {
+            column: true,
+            sm: 2
+        },
+        "field" : {
+
+        },
+        "column" : {
+            sm: 10
+        },
+        "textbelow" : {
+            className: "text-muted"
+        }
+    }
+
+
+    const formik = useFormik({
+        initialValues: {
+          name: '',
+        },
+        onSubmit: values => {
+          alert(JSON.stringify(values, null, 2));
+        },
+      });
+
+    return(<Form noValidate
+        onSubmit={formik.handleSubmit}
+        >
+        <TextField name="name" value={formik.values.name} onChange={formik.handleChange} placeholder="Enter Name" label="Name:" customParameters={customParameters} required {...props} />
+        <PasswordField name="password" placeholder="Enter Password" label="Password:" customParameters={customParameters} {...props} />
+        <PasswordField name="confirm_password" placeholder="Confirm Password" label="Confirm Password:" customParameters={customParameters} required {...props} />
+        <Button type="submit">Submit form</Button>
+        </Form>)
+}
