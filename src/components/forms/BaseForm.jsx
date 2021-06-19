b7250049ffa42c9c3fb32b016f5367493062ac68
Febin Baiju <febin.baiju@orisys.in>
2021-06-19 14:51:55 +0530

    Integrated BaseForm component with text fields and buttons and made it functional

--- src/components/forms/BaseForm.jsx
+++ src/components/forms/BaseForm.jsx
@@ -1,20 +1,37 @@
 import { useFormik } from "formik";
+import React from "react";
 import { Form } from "react-bootstrap";
 
 export const BaseForm = (
     {
+      fields = {},
         ...props        
     }
 ) => {
-    const formik = useFormik({
-        onSubmit: values => {
-          alert(JSON.stringify(values, null, 2));
-        },
-      });
+
+  const form = useFormik({
+    initialValues: fields,
+      onSubmit: values => {
+        alert(JSON.stringify(values, null, 2));
+      },
+    });
+
+  function form_fields(element, index)
+  {
+    switch(element.type.name)
+    {
+      case "TextField":
+        return(React.cloneElement(element, {onChange: form.handleChange, value: form.values.name, key: index}))
+      default:
+        return React.cloneElement(element, { key: index })
+    }
+  }
 
       return(<Form noValidate
-        onSubmit={formik.handleSubmit}
+        onSubmit={form.handleSubmit}
         >
-            {props.children}
+          {props.children.map((element, index) => {
+            return(form_fields(element, index))
+          })}
         </Form>)
 }
