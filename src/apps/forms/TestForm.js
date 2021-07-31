165cfd100f8c88cf57fee2dd6d675a12a0ae75b6
Febin Baiju <febin129@gmail.com>
2021-07-31 13:38:19 +0530

    Update TestForm.js

--- src/apps/forms/TestForm.js
+++ src/apps/forms/TestForm.js
@@ -59,39 +59,42 @@ export default function TestForm(props) {
   }, [form_values]);
 
   return (
-    <BaseForm
-      fields={fields}
-      multiple_fields_validation_rules={validation_parameters}
-      set_form_values={setFormValues}
-      individual_fields_validation_rules={single_element_validation}
-    >
-      <TextField
-        name="first_name"
-        placeholder="Enter First Name"
-        label="First Name"
-        customParameters={customParameters}
-        required
-        min={5}
-        //max={10}
-        {...props}
-      />
-      <TextField
-        name="last_name"
-        placeholder="Enter Last Name"
-        label="Last Name"
-        customParameters={customParameters}
-        required
-        max={10}
-        {...props}
-      />
-      <TextField
-        name="address"
-        placeholder="Enter Address"
-        label="Address"
-        customParameters={customParameters}
-        {...props}
-      />
-      <Button type="submit">Submit form</Button>
-    </BaseForm>
+    <>
+      <h3>Example Form</h3>
+      <BaseForm
+        fields={fields}
+        multiple_fields_validation_rules={validation_parameters}
+        set_form_values={setFormValues}
+        individual_fields_validation_rules={single_element_validation}
+      >
+        <TextField
+          name="first_name"
+          placeholder="Enter First Name"
+          label="First Name"
+          customParameters={customParameters}
+          required
+          min={5}
+          //max={10}
+          {...props}
+        />
+        <TextField
+          name="last_name"
+          placeholder="Enter Last Name"
+          label="Last Name"
+          customParameters={customParameters}
+          required
+          max={10}
+          {...props}
+        />
+        <TextField
+          name="address"
+          placeholder="Enter Address"
+          label="Address"
+          customParameters={customParameters}
+          {...props}
+        />
+        <Button type="submit">Submit form</Button>
+      </BaseForm>
+    </>
   );
 }
